27d17bcd82b4addcfd01e53a35a499f7334c3a2b
KimCookieYa <min49590@gmail.com>
2023-12-01 03:51:02 +0900

    Feat: 노션 홈 임베딩

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import 'react-notion-x/src/styles.css';
+import 'prismjs/themes/prism-tomorrow.css';
+import 'katex/dist/katex.min.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,32 @@
-export default function Home() {
-  return <div>Hello, World!</div>;
+import type { InferGetStaticPropsType, GetStaticProps } from 'next';
+import { NotionAPI } from 'notion-client';
+import { ExtendedRecordMap } from 'notion-types';
+import { NotionRenderer } from 'react-notion-x';
+import { Collection } from 'react-notion-x/build/third-party/collection';
+
+export default function Home(props: InferGetStaticPropsType<typeof getStaticProps>) {
+  const recordMap = props.recordMap;
+  return (
+    <div>
+      Hello, World!
+      <NotionRenderer
+        recordMap={recordMap}
+        fullPage
+        darkMode={false}
+        components={{
+          Collection,
+        }}
+      />
+    </div>
+  );
 }
+
+export const getStaticProps = (async () => {
+  const notion = new NotionAPI();
+  const recordMap: ExtendedRecordMap = await notion.getPage('c2f734b31938417d879063c831fb2759');
+  return {
+    props: {
+      recordMap,
+    },
+  };
+}) satisfies GetStaticProps<{ recordMap: ExtendedRecordMap }>;
