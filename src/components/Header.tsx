679d17b6bdc101aa58ac996c12124830835510f8
KimCookieYa <min49590@gmail.com>
2024-01-08 16:39:32 +0900

    Design: 헤더에 스크롤 애니메이션 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,46 @@
 import Link from 'next/link';
 import { routerData } from './router/route';
 import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
 
 export default function Header() {
   const router = useRouter();
 
+  const [scroll, setScroll] = useState(0);
+
+  const handleScroll = () => {
+    const position = window.scrollY;
+    const totalHeight = document.body.scrollHeight - window.innerHeight;
+    const progress = (position / totalHeight) * 100;
+
+    setScroll(progress);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="fixed flex h-12 w-full top-0 bg-white items-center z-[99999]">
+    <header className="fixed flex flex-col h-12 w-full top-0 bg-white items-center z-[99999]">
       <nav className="flex w-full justify-around">
         {routerData.map((item) => {
+          if (!item.isNotion) {
+            return (
+              <a href={item.path} key={item.label} target='_blank'>
+                <button
+                  className={[
+                    'px-4 py-2 hover:text-blue-500 hover:border-b-2 hover:border-blue-500',
+                    router.pathname === item.path ? ' font-bold' : '',
+                  ].join(' ')}
+                >
+                  {item.label}
+                </button>
+              </a>)
+          }
+
           return (
             <Link href={item.path} key={item.label}>
               <button
@@ -23,6 +55,10 @@ export default function Header() {
           );
         })}
       </nav>
+      <div
+        className="h-1 bg-blue-500 origin-left w-full"
+        style={{ transform: `scaleX(${scroll /100})` }}
+      ></div>
     </header>
   );
 }
